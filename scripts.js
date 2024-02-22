dc0a8ea08f1a437ce0ce25c9d5549733477fb2b2
arthur <artyr007@gmail.com>
2024-02-23 00:25:12 +0500

    Fix maxlength and minlength of password validation

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
-const signupButton = document.getElementById("signup-btn");
-const passwordInput = document.getElementById("new-password");
-const confirmPasswordInput = document.getElementById("confirm-password");
-const errorMsg = document.querySelector("#new-password+span");
+    const signupButton = document.getElementById("signup-btn");
+    const passwordInput = document.getElementById("new-password");
+    const confirmPasswordInput = document.getElementById("confirm-password");
+    const errorMsg = document.querySelector("#new-password+span");
 
-function validatePwd(event) {
-  const form = event.target.form;
-  if (
-    passwordInput.value &&
-    confirmPasswordInput.value &&
-    form.checkValidity()
-  ) {
-    if (passwordInput.value === confirmPasswordInput.value) {
-      form.submit();
-    } else {
-      event.preventDefault();
-      errorMsg.classList.add("error");
+    function validatePwd(event) {
+    const form = event.target.form;
+    if (
+        passwordInput.value &&
+        confirmPasswordInput.value &&
+        form.checkValidity()
+    ) {
+        if (passwordInput.value === confirmPasswordInput.value) {
+        form.submit();
+        } else {
+        event.preventDefault();
+        errorMsg.classList.add("error");
+        }
     }
-  }
-}
+    }
 
-signupButton.addEventListener("click", validatePwd);
+    signupButton.addEventListener("click", validatePwd);
